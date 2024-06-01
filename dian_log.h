dfb563182aca49b5d608071d4e49cacca2a8eaee
dianyuluo <1739556970@qq.com>
2024-06-01 15:45:41 +0000

    init

---
+++ dian_log.h
@@ -0,0 +1,18 @@
+#ifndef DIAN_LOG_H
+#define DIAN_LOG_H
+
+#pragma once
+#include <string>
+using namespace std;
+class DianLog
+{
+public:
+    DianLog();
+    ~DianLog();
+    void logRed(string);
+    void logRedLine();
+
+private:
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <dian_log.h>
+int main(int argc, char const *argv[])
+{
+    DianLog dianlog;
+    dianlog.logRedLine();
+    dianlog.logRedLine();
+    return 0;
+}
